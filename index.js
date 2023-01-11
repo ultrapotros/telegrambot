29fdb44f94e1616169abb431a3a764837823bc8b
santi <santimezquita@hotmail.es>
2023-01-11 15:01:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const {Telegraf} = require('telegraf');
+const {dockStart} = require('@nlpjs/basic');
+
+require('dotenv').config();
+
+const app = express();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// bot info santi_mezquita_bot
+// username ultrapotros_bot
+
+app.use(bot.webhookCallback('/url/telegram'));
+bot.telegram.setWebhook(`${process.env.BOT_URL}/url/telegram`);
+
+app.post('/url/telegram', (req, res)=> {
+
+    res.send('termina');
+})
+
+bot.on('text', async (ctx)=> {
+    const dock = await dockStart();
+    const nlp = dock.get('nlp');
+    const response = await nlp.process('es',ctx.message.text);
+    if (response.answer) {
+        ctx.reply(response.answer);
+    } else {
+        ctx.reply('Perdona no te entiendo. Por ahora solo se saludar y despedirme')
+    }
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, async ()=> {
+    console.log(`working in port:${PORT}`);
+    const dock = await dockStart();
+    const nlp = dock.get('nlp');
+    await nlp.train();
+})
